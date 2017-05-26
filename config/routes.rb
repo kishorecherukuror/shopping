8d201b3c2fff1b0d9e3c5ac18c80d0c173bf7e86
dathuk <dathu.k@mdoffice.in>
2017-05-26 08:12:00 +0530

    routing changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :products, only: [:new,:create] do
+  resources :products, only: [:new,:create, :edit ] do
   	member do
-  		get 'category_product'
+  		get 'new_category_product'
   	end
   	collection do
   		post 'create_product'
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   end
 
 
-  resources :categories
+  resources :categories do
+    member do
+      get 'products_list', controller: :products
+    end
+  end
   get 'welcome/index'
   root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
